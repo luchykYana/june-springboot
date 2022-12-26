2843773d9cc6d3c961c1699a09e6e4b146944b3f
yana <yanaluchyk@gmail.com>
2022-12-26 22:08:14 +0200

    init

---
+++ src/main/java/com/example/junespringboot/JuneSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.junespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuneSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuneSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/junespringboot/JuneSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuneSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
